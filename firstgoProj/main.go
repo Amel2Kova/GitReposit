894a2ef92ca664d51f71ba737d5f74737f6e8fa5
Amel2Kova <amelsmajic189@gmail.com>
2022-11-02 15:31:24 +0100

    Update main.go

--- firstgoProj/main.go
+++ firstgoProj/main.go
@@ -5,261 +5,262 @@ import (
 )
 
 /*
-1.	Omogućiti operacije unosa, izmjene, pregleda i brisanja:članova, radnika, knjiga, književnog žanra, polica za knjige, kategorija članarina, te posuđenih knjiga.
-2.	Unos novih stavki, te izmjenu postojećih vršiti sa tastature.
-3.	Željenu radnju (unos, izmjena, pregled, brisanje) korisnik unosi preko tastature na osnovu ponuđenih opcija u terminalu.
-4.	Za novi unos definirati potrebne kolone i ograničiti unos ukoliko se ne unesu potrebni podaci.
-5.	Za povezivanje tabela koristit će se ključevi, koji predstavljaju jedinstveni identifikator određene stavke, kao npr:
--	radnik/član: tekstualni zapis JMBG od 13 cifara,
--	knjiga: tekstualni zapis od 5 znakova, gdje 1. znak označava žanr (B1010, K5425, itd.),
--	polica: tekstualni zapis sa oznakom žanra i numeracijom police;
-6.	Napraviti dokumentaciju projekta u obliku Word dokumenta. Koristiti ovaj dokument kao šablon, te početi sa 5. poglavljem svoj izvještaj o projektu.
+1. Enable operations of entering, changing, viewing and deleting: members, workers, books, literary genre, bookshelves, categories of membership fees, and borrowed books.
+2. Enter new items and change existing ones from the keyboard.
+3. The desired action (entry, modification, review, deletion) is entered by the user via the keyboard based on the options offered in the terminal.
+4. For a new entry, define the required columns and limit the entry if the required data is not entered.
+5. To connect the tables, keys will be used, which represent the unique identifier of a certain item, such as:
+- worker/member: text record JMBG of 13 digits,
+- book: text record of 5 characters, where the 1st character indicates the genre (B1010, K5425, etc.),
+- shelf: text record with genre label and shelf numbering;
+6. Create project documentation in the form of a Word document. Use this document as a template, and start with Chapter 5 of your project report.
 */
 
-type covjek struct {
-	ime     string
-	prezime string
-	godiste string
-	JMBG    string
+type man struct {
+	name     string
+	lastname string
+	year     string
+	JMBG     string
 }
 
-type clan struct {
-	covjek
-	podignuteKnjige    []string
-	limitNaDizanje     uint8
-	trenutnoPodignutih uint8
+type member struct {
+	man
+	limitUpRaise    uint8
+	currentlyRaised uint8
 }
-type radnik struct {
-	covjek
-	pozicija string
-	smjena   string
+type worker struct {
+	man
+	position string
+	shift    string
 }
-type knjiga struct {
-	naziv  string
-	autor  string
+type book struct {
+	title  string
+	author string
 	ID     string
 	Status string
 }
-type zanr struct {
-	NazivZanra string
-	ZnakZanra  string
+type genre struct {
+	NameZanra string
+	ZnakZanra string
 }
 
-type polica struct {
-	zanr  string
+type shelf struct {
+	genre string
 	index int
 }
 
-var Clanovi = []clan{
-	{
-		covjek:             covjek{ime: "Amel", prezime: "Smajic", godiste: "2002", JMBG: "2102200286041"},
-		podignuteKnjige:    []string{""},
-		limitNaDizanje:     3,
-		trenutnoPodignutih: 0,
-	},
-	{
-		covjek: covjek{
-			ime:     "Armin",
-			prezime: "Avdic",
-			godiste: "1999",
-			JMBG:    "120419991234",
-		},
-		podignuteKnjige:    []string{},
-		limitNaDizanje:     3,
-		trenutnoPodignutih: 0,
-	},
-}
-
-var Radnici = []radnik{
-	{
-		covjek: covjek{
-			ime:     "Tim",
-			prezime: "tibersom",
-			godiste: "1982",
-			JMBG:    "19820312310293",
-		},
-		pozicija: "Upravitelj",
-		smjena:   "Prva",
-	}, {
-		covjek: covjek{
-			ime:     "Andrey",
-			prezime: "Robines",
-			godiste: "1999",
-			JMBG:    "2102199991234",
-		},
-		pozicija: "Bibliotekar",
-		smjena:   "Prva",
-	}, {
-		covjek: covjek{
-			ime:     "Michael",
-			prezime: "Jeas",
-			godiste: "1983",
-			JMBG:    "120319839213",
-		},
-		pozicija: "Domar",
-		smjena:   "Prva",
-	},
-}
-
-var Knjige = []knjiga{
-	{
-		naziv:  "The Lion, the Witch and the Wardrobe",
-		autor:  "C. S. Lewis",
-		ID:     "",
-		Status: "na raspolaganju",
-	}, {
-		naziv:  "She: A History of Adventure",
-		autor:  "H. Rider Haggard",
-		ID:     "",
-		Status: "na raspolaganju",
-	}, {
-		naziv:  "The Da Vinci Code",
-		autor:  "Den Brown",
-		ID:     "",
-		Status: "na raspolaganju",
-	},
-}
-var Zanrovi = []zanr{
-	{
-		NazivZanra: "Fantazy",
-		ZnakZanra:  "F",
-	},
-	{
-		NazivZanra: "Adventure",
-		ZnakZanra:  "A",
-	},
-	{
-		NazivZanra: "History",
-		ZnakZanra:  "H",
-	},
-	{
-		NazivZanra: "Thriller",
-		ZnakZanra:  "T",
-	},
-}
-
-var Police = []polica{
-	{
-		zanr:  "Fantazy",
-		index: 0000,
-	},
-	{
-		zanr:  "Adventure",
-		index: 0001,
-	},
-	{
-		zanr:  "History",
-		index: 0002,
-	},
-	{
-		zanr:  "Thriller",
-		index: 0003,
-	},
-}
-
 func main() {
-	var active = true
+	Members := []member{
+		{
+			man: man{name: "Amel", lastname: "Smajic", year: "2002", JMBG: "2102200286041"},
 
+			limitUpRaise:    3,
+			currentlyRaised: 0,
+		},
+		{
+			man: man{
+				name:     "Armin",
+				lastname: "Avdic",
+				year:     "1999",
+				JMBG:     "120419991234",
+			},
+
+			limitUpRaise:    3,
+			currentlyRaised: 0,
+		},
+	}
+
+	Workers := []worker{
+
+		{
+			man: man{
+				name:     "Team",
+				lastname: "tibersom",
+				year:     "1982",
+				JMBG:     "19820312310293",
+			},
+			position: "Manager",
+			shift:    "First",
+		}, {
+			man: man{
+				name:     "Andrey",
+				lastname: "Robines",
+				year:     "1999",
+				JMBG:     "2102199991234",
+			},
+			position: "Librarian",
+			shift:    "First",
+		}, {
+			man: man{
+				name:     "Michael",
+				lastname: "Jeas",
+				year:     "1983",
+				JMBG:     "120319839213",
+			},
+			position: "Caretaker",
+			shift:    "First",
+		},
+	}
+
+	Books := []book{
+		{
+			title:  "The Lion, the Witch and the Wardrobe",
+			author: "C.S. Lewis",
+			ID:     "",
+			Status: "available",
+		}, {
+			title:  "She: A History of Adventure",
+			author: "H. Rider Haggard",
+			ID:     "",
+			Status: "available",
+		}, {
+			title:  "The Da Vinci Code",
+			author: "Dan Brown",
+			ID:     "",
+			Status: "available",
+		},
+	}
+	Genres := []genre{
+		{
+			NameZanra: "Fantasy",
+			ZnakZanra: "F",
+		},
+		{
+			NameZanra: "Adventure",
+			ZnakZanra: "A",
+		},
+		{
+			NameZanra: "History",
+			ZnakZanra: "H",
+		},
+		{
+			NameZanra: "Thriller",
+			ZnakZanra: "T",
+		},
+	}
+
+	shelves := []shelf{
+		{
+			genre: "Fantasy",
+			index: 0000,
+		},
+		{
+			genre: "Adventure",
+			index: 0001,
+		},
+		{
+			genre: "History",
+			index: 0002,
+		},
+		{
+			genre: "Thriller",
+			index: 0003,
+		},
+	}
+
+	var active = true
 	for active {
-		var opcija = 0
+		var options = 0
 		//fmt.Printf("")
-		fmt.Printf("Dobro dosli u biblioteku\n")
-		fmt.Printf("Molimo odaberite bazu koju trebate urediti\n")
-		fmt.Printf("1.) Korisnika\n")
-		fmt.Printf("2.) Zaposlenika\n")
-		fmt.Printf("3.) Knjiga\n")
-		fmt.Printf("4.) Zanrova\n")
-		fmt.Printf("5.) Polica\n")
+		fmt.Printf("Welcome to the library\n")
+		fmt.Printf("Please select the database you need to edit\n")
+		fmt.Printf("1.) User\n")
+		fmt.Printf("2.) Employees\n")
+		fmt.Printf("3.) Book\n")
+		fmt.Printf("4.) Genre\n")
+		fmt.Printf("5.) Shelf\n")
 		fmt.Printf("6.) Exit\n")
 
-		fmt.Scan(&opcija)
+		fmt.Scan(&options)
 
-		switch opcija {
+		switch options {
 
 		case 1:
-			korisnikf()
+			userf(Members)
 
 		case 2:
-			radnikf()
+			workerf(Workers)
 
 		case 3:
-			knjigaf()
+			bookf(Books)
 
 		case 4:
-			zanrf()
+			genref(Genres)
 
 		case 5:
-			policaf()
+			shelff(shelves)
 
 		case 6:
 			active = false
 
 		default:
-			errore()
+			error()
 		}
 	}
 
 }
-func errore() {
 
-	fmt.Printf("Molimo unesite tacan podatak \n")
+func error() {
+
+	fmt.Printf("Please enter correct data \n")
 }
 
-func Manu2(baza string) (opcija2 int) {
+func Manu2(base string) (option2 int) {
 
-	fmt.Printf("Dobro dosli u bazu %v \n", baza)
-	fmt.Printf("Molimo odaberite zeljenu opciju\n")
-	fmt.Printf("1.) Unesi %v\n", baza)
-	fmt.Printf("2.) Izmijeni %v\n", baza)
-	fmt.Printf("3.) Pregledaj %v\n", baza)
-	fmt.Printf("4.) Izbrisi %v\n", baza)
+	fmt.Printf("Welcome to database %v \n", base)
+	fmt.Printf("Please select the desired option\n")
+	fmt.Printf("1.) Enter %v\n", base)
+	fmt.Printf("2.) Modify %v\n", base)
+	fmt.Printf("3.) Browse %v\n", base)
+	fmt.Printf("4.) Delete %v\n", base)
 	fmt.Printf("5.) Exit")
-	fmt.Scan(&opcija2)
+	fmt.Scan(&option2)
 
 	return
 }
 
-func korisnikf() {
+func userf(User []member) {
 
 	var active bool = true
 	for active {
 
-		var opcija = Manu2("Korisnika")
-		fmt.Printf("odabir je: %v", opcija)
+		var option = Manu2("User")
+		fmt.Printf("selection is: %v", option)
 
-		switch opcija {
+		switch option {
 		case 1:
-			UnosKorisnika()
+			UserInput(User)
 		case 2:
 
 		case 3:
-			PregledajKorisnike()
+			BrowseUsers(User)
 		case 4:
 
 		case 5:
 			active = false
 
 		default:
-			errore()
+			error()
 		}
 	}
 }
 func line() {
-	fmt.Printf("\n-------------------------------------------------------------------------------\n")
+	fmt.Printf("\n------------------------------------------------ ------------------------------------\n")
 
 }
-func PregledajKorisnike() {
-	for i, val := range Clanovi {
+
+func BrowseUsers(Members []member) {
+	for i, val := range Members {
 		line()
-		fmt.Printf("\nIme korisnika je: %v \n", val.ime)
-		fmt.Printf("Prezime korisnika je: %v \n", val.prezime)
-		fmt.Printf("Godiste korisnika je: %v \n", val.godiste)
-		fmt.Printf("Jmbg korisnika je: %v \n", val.JMBG)
-		if val.trenutnoPodignutih == 0 {
-			fmt.Printf("Korisnik nema podignutih knjiga i moze da podigne jos %v knjiga\n\n", val.limitNaDizanje)
+		fmt.Printf("\nUser name is: %v \n", val.name)
+		fmt.Printf("The user's last name is: %v \n", val.lastname)
+		fmt.Printf("The user's age is: %v \n", val.year)
+		fmt.Printf("User jmbg is: %v \n", val.JMBG)
+		if val.currentlyRaised == 0 {
+			fmt.Printf("The user has no uploaded books and can upload %v books\n\n", val.limitUpRaise)
 
 		} else {
-			fmt.Printf("Korisnik ima %v podignutih knjiga i moze podignuti %v jos\n\n ", val.trenutnoPodignutih, (val.limitNaDizanje - val.trenutnoPodignutih))
+			fmt.Printf("User has %v of books uploaded and can upload %v more\n\n ", val.currentlyRaised, (val.limitUpRaise - val.currentlyRaised))
 
 		}
 		line()
@@ -268,172 +269,174 @@ func PregledajKorisnike() {
 
 }
 
-func UnosKorisnika() {
-	var n = len(Clanovi)
-	fmt.Printf("\nMolimo unesite podatke novog Korisnika : \n")
+func UserInput(Members []member) {
+	var n = len(Members)
+	fmt.Printf("\nPlease enter the information of the new User : \n")
 	line()
-	fmt.Printf("Ime korisnika : \n")
-	fmt.Scanf(Clanovi[n].ime)
-	fmt.Printf("Prezime korisnika : \n")
-	fmt.Scanf(Clanovi[n].prezime)
-	fmt.Printf("Godiste korisnika :\n")
-	fmt.Scanf(Clanovi[n].godiste)
-	fmt.Printf("JMBG korisnika : \n")
-	fmt.Scanf(Clanovi[n].JMBG)
-	fmt.Printf("Unesite limit na dizanje (int) : \n")
-	fmt.Scan(Clanovi[n].limitNaDizanje)
+	var name string
+	fmt.Printf("User name : \n")
+	fmt.Scanf(name)
+	Members[n].name = name
+	fmt.Printf("User last name : \n")
+	fmt.Scanf(Members[n].lastname)
+	fmt.Printf("User age :\n")
+	fmt.Scanf(Members[n].year)
+	fmt.Printf("User JMBG : \n")
+	fmt.Scanf(Members[n].JMBG)
+	fmt.Printf("Enter lifting limit (int) : \n")
+	fmt.Scan(Members[n].limitUpRaise)
 
 }
-func radnikf() {
+func workerf(Worker []worker) {
 
 	var active = true
 	for active {
-		var opcija = Manu2("Zaposlenika")
+		var option = Manu2("Employee")
 
-		switch opcija {
+		switch option {
 
 		case 1:
 
 		case 2:
 
 		case 3:
-			pregledRadnika()
+			viewWorker(Worker)
 		case 4:
 
 		case 5:
 			active = false
 
 		default:
-			errore()
+			error()
 		}
 	}
 }
 
-func pregledRadnika() {
-	fmt.Printf("\nLista radnika:\n")
+func viewWorker(Workers []worker) {
+	fmt.Printf("\nList of workers:\n")
 	line()
 
-	for i, val := range Radnici {
+	for i, val := range Workers {
 		line()
-		fmt.Printf("\nIme : %v\n", val.ime)
-		fmt.Printf("Prezime : %v\n", val.prezime)
-		fmt.Printf("Godiste : %v\n", val.godiste)
+		fmt.Printf("\nName : %v\n", val.name)
+		fmt.Printf("Surname : %v\n", val.lastname)
+		fmt.Printf("Year : %v\n", val.year)
 		fmt.Printf("Jmbg : %v\n", val.JMBG)
-		fmt.Printf("Pozicja : %v\n", val.pozicija)
-		fmt.Printf("Smjena : %v\n", val.smjena)
+		fmt.Printf("Position : %v\n", val.position)
+		fmt.Printf("Shift : %v\n", val.shift)
 		line()
 		i++
 	}
 
 }
 
-func knjigaf() {
+func bookf(Books []book) {
 
 	var active = true
 	for active {
 
-		var opcija = Manu2("Knjigu")
-		switch opcija {
+		var option = Manu2("Book")
+		switch option {
 		case 1:
 
 		case 2:
 
 		case 3:
-			PregledKnjiga()
+			ViewBook(Books)
 		case 4:
 
 		case 5:
 			active = false
 
 		default:
-			errore()
+			error()
 		}
 	}
 }
 
-func PregledKnjiga() {
-	fmt.Printf("\nLista knjiga : \n")
+func ViewBook(Books []book) {
+	fmt.Printf("\nList of books : \n")
 	line()
-	for i, val := range Knjige {
+	for i, val := range Books {
 
-		fmt.Printf("\nNaziv knjige je : %v\n", val.naziv)
-		fmt.Printf("Autor knjige je : %v\n", val.autor)
-		fmt.Printf("Id knjige je : %v\n", val.ID)
-		fmt.Printf("Status knjige je : %v\n", val.Status)
+		fmt.Printf("\nThe name of the book is : %v\n", val.title)
+		fmt.Printf("The author of the book is : %v\n", val.author)
+		fmt.Printf("Id of the book is : %v\n", val.ID)
+		fmt.Printf("Book status is : %v\n", val.Status)
 		line()
 		i++
 	}
 
 }
 
-func zanrf() {
+func genref(Genres []genre) {
 
 	var active bool = true
 	for active {
-		var opcija = Manu2("Zanr")
+		var option = Manu2("Genre")
 
-		switch opcija {
+		switch option {
 		case 1:
 
 		case 2:
 
 		case 3:
-			PregledZanrova()
+			GenreView(Genres)
 		case 4:
 
 		case 5:
 			active = false
 
 		default:
-			errore()
+			error()
 		}
 	}
 }
 
-func PregledZanrova() {
-	fmt.Printf("\nPregled zanrova\n")
+func GenreView(Genres []genre) {
+	fmt.Printf("\nView of genres\n")
 	line()
 
-	for i, val := range Zanrovi {
-		fmt.Printf("NAziv zanra je : %v \n", val.NazivZanra)
-		fmt.Printf("Kod zanra je : %v \n\n", val.ZnakZanra)
+	for i, val := range Genres {
+		fmt.Printf("Genre NAME is : %v \n", val.NameZanra)
+		fmt.Printf("Genre code is : %v \n\n", val.ZnakZanra)
 		line()
 
 		i++
 	}
 
 }
-func policaf() {
+func shelff(Shelf []shelf) {
 
 	var active = true
 	for active {
-		var opcija = Manu2("Policu")
+		var option = Manu2("Shelf")
 
-		switch opcija {
+		switch option {
 
 		case 1:
 
 		case 2:
 
 		case 3:
-			PregledPolica()
+			ViewShelf(Shelf)
 		case 4:
 
 		case 5:
 			active = false
 
 		default:
-			errore()
+			error()
 		}
 	}
 }
-func PregledPolica() {
+func ViewShelf(Shelf []shelf) {
 
-	fmt.Printf("\n\nPregled polica\n")
+	fmt.Printf("\n\nList of shelves\n")
 	line()
-	for i, val := range Police {
-		fmt.Printf("Zanr police je: %v\n", val.zanr)
-		fmt.Printf("Index police je : %v\n", val.index)
+	for i, val := range Shelf {
+		fmt.Printf("The genre of the shelf is: %v\n", val.genre)
+		fmt.Printf("Shelf index is : %v\n", val.index)
 		line()
 		i++
 	}
